819e9a16879b1f7e3fd6d78809e86d78dcacea45
Raquel Levy <levy.r@husky.neu.edu>
2018-10-15 17:50:55 -0400

    add user resource

--- lib/taskTracker_web/router.ex
+++ lib/taskTracker_web/router.ex
@@ -18,6 +18,7 @@ defmodule TaskTrackerWeb.Router do
 
     get "/", PageController, :index
     resources "/taskitems", TaskitemController
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
